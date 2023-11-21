3904469825b0419777b087858664929c5f6b6428
Chris <rolletchris1@gmail.com>
2023-11-20 18:43:07 -0800

    fix background color issue for dark mode

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,13 +6,13 @@ import Footer from '@/components/footer'
 
 export default function Page() {
   return (
-    <div className="h-screen p-8 lg:p-16 mt-16 bg-gray-100">
+    <div className="h-screen p-8 lg:p-16 mt-16">
       <Menu />
       <div className="p-6">
         <div className="p-5">
           <h5 className="mb-2 text-2xl font-bold tracking-tight">Projects</h5>
           <p className="mb-3 font-normal">
-            In construction...
+            Coming Soon...
           </p>
         </div>
       </div>
