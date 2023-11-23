23631ee9a830e7d079279d3b74426a1daf137370
Chris <rolletchris1@gmail.com>
2023-11-22 18:20:18 -0800

    Update projects to add deep work timer

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,9 +12,19 @@ export default function Page() {
         <div className="p-1">
           <h5 className="mb-2 text-2xl font-bold tracking-tight">Projects</h5>
           <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
-          <p className="mb-3 font-normal">
-            Coming Soon...
-          </p>
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
+            <a href="/deep-work">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Deep Work Timer</h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700">
+              Deep Working is a technique that helps you focus on a single task for a set amount of time. This timer helps you do just that!
+            </p>
+            <div className="flex justify-center">
+              <a href="/deep-work" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                Try it!
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
