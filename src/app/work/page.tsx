202edd84b937a1b338ba7dc6f06fbdad5af8d2fc
Chris <rolletchris1@gmail.com>
2023-11-22 18:18:53 -0800

    update button color

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -11,12 +11,12 @@ export default function Page() {
       <div className="flex items-center justify-center">
         <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ">
           <a href="/work/uber">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Uber ~ Software Engineering</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Uber ~ Software Engineering</h5>
           </a>
           <p className="mb-3 font-normal text-gray-700">
             First summer internship as a software engineer with Uber and Growth Platform/DOCTOM Team.
           </p>
-          <a href="/work/uber" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+          <a href="/work/uber" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
             Read more
             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
