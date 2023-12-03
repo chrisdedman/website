08661db09a0ece0c62121d0022e8bd2b61c18c03
Chris <rolletchris1@gmail.com>
2023-12-03 15:43:44 -0800

    update button color

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -60,7 +60,7 @@ function AboutMe() {
           I am constantly seeking out opportunities to learn and grow, and I am
           looking for internship opportunities that will allow me to gain practical experience in the field.
         </p>
-        <a href="mailto:cdedmanrollet1@toromail.csudh" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <a href="mailto:cdedmanrollet1@toromail.csudh" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Email Me
           <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
