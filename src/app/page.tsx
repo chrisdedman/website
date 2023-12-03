176362714a436a3285713df7107f6988ed2b9950
Chris <rolletchris1@gmail.com>
2023-12-03 15:44:13 -0800

    added categories homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client"
 import React from "react";
 import Menu from "@/components/navigation";
 import { RocketLaunchIcon } from '@heroicons/react/24/solid';
 
 export default function Page() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex flex-col items-center justify-center">
       <Menu />
+
       <div className="text-center rocket-container">
         <RocketLaunchIcon className="w-8 h-8 rocket spin-on-hover" />
         <h1 className="text-5xl font-bold">Hi, This is</h1>
@@ -16,6 +16,18 @@ export default function Page() {
         </div>
         <p className="text-2xl font-light mb-8">Computer Scientist Student</p>
       </div>
+
+      <div className="flex flex-wrap justify-center">
+        <CategoryCard title="Profile" />
+        <CategoryCard title="Work" />
+        <CategoryCard title="Projects" />
+      </div>
     </div>
   );
 }
+
+const CategoryCard = ({ title }: { title: string }) => (
+  <span className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+    <a href={title.toLowerCase()} className=" hover:underline">{title}</a>
+  </span>
+);
