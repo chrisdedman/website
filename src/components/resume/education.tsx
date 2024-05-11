5b6c6e57dda99a523aa77a10a8eb548a851f3c62
Chris <rolletchris1@gmail.com>
2024-05-10 22:56:27 -0700

    Refactor education component structure and content

--- src/components/resume/education.tsx
+++ src/components/resume/education.tsx
@@ -2,32 +2,29 @@ import React from "react";
 
 export default function Educations() {
   return (
-    <div className="p-1 mt-6">
-      <div className="p-0">
-        <h5 className="mb-2 text-m font-bold tracking-tight">Educations</h5>
-        <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
-        <div className="mb-3 font-normal">
-          <strong>California State University, Dominguez Hills</strong>
-          <p className="text-sm font-light">Bachelor of Science, Computer Science</p>
-          <p className="text-sm font-light italic">January 2023 - May 2025</p>
-          <p className="text-sm font-light font-montez">
-            As a student at California State University, Dominguez Hills, I am pursuing a Bachelor of Science degree in Computer Science.
-            I have completed several computer science courses, including Software Development in java, Data Structure, and Computer Organisation.
-            I have also completed Probability and Statistics course.
-            I am currently taking courses in Data Management, Analysis of Algorithms, Programming Languages Concepts and Physics 1.
-          </p>
-        </div>
+    <div className="p-1">
+      <h5 className="mb-2 text-m font-bold tracking-tight">Educations</h5>
+      <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
+      <div className="mb-3 font-normal">
+        <strong>California State University, Dominguez Hills</strong>
+        <p className="text-sm font-light">Bachelor of Science, Computer Science</p>
+        <p className="text-sm font-light italic">January 2023 - May 2025</p>
+        <p className="text-sm font-light font-montez">
+          As a student at California State University, Dominguez Hills, I am pursuing a Bachelor of Science degree in Computer Science.
+          I have completed several computer science courses, including Software Development in java, Data Structure, Design and Analysis of Algorithms, Database Management and Computer Organisation.
+          I have also completed Probability & Statistics, and Physics 1 courses.
+        </p>
+      </div>
 
-        <div className="mb-3 font-normal">
-          <strong>El Camino College</strong>
-          <p className="text-sm font-light">Certificate of Achievement, Computer Science</p>
-          <p className="text-sm font-light italic">July 2021 - June 2023</p>
-          <p className="text-sm font-light font-montez">
-            As a student at El Camino College, I have completed several computer science courses,
-            including Data Structures and Algorithms, Assembly Language, and Intro to C++, Intro do Java.
-            I have also completed several courses, including English, Calculus 1 & 2, Discret Mathemathics, and Cisco Network Engineering (CCNA).
-          </p>
-        </div>
+      <div className="mb-3 font-normal">
+        <strong>El Camino College</strong>
+        <p className="text-sm font-light">Certificate of Achievement, Computer Science</p>
+        <p className="text-sm font-light italic">July 2021 - June 2023</p>
+        <p className="text-sm font-light font-montez">
+          As a student at El Camino College, I have completed several computer science courses,
+          including Data Structures and Algorithms, Assembly Language, and Intro to C++, Intro do Java.
+          I have also completed several courses, including English, Calculus 1 & 2, Discret Mathemathics, and Cisco Network Engineering (CCNA).
+        </p>
       </div>
     </div>
   );
