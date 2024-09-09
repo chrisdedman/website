e4916368d1ce54fde01d013fc888effc5a6d98f3
Chris <rolletchris1@gmail.com>
2024-09-08 18:43:47 -0700

    refactor: Update experience component with Uber internship details and add previous work experience
    - Updated the experience component to include detailed information about my Software/Data Engineering Internship at Uber.
    - Added previous work experience in the French Military and as a butcher in France and Belgium.

--- src/components/resume/experience.tsx
+++ src/components/resume/experience.tsx
@@ -7,15 +7,21 @@ export default function Experiences() {
       <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
       <div className="mb-3 font-normal">
         <strong>Uber</strong>
-        <p className="text-sm font-light">Software Engineering Intern</p>
+        <p className="text-sm font-light">Software/Data Engineering Intern</p>
         <p className="text-sm font-light italic">May 2024 - August 2024</p>
         <p className="text-sm font-light font-montez">
-          Intern as software engineer during summer 2024 with the AdTech team at Uber.
+          During my second internship as a Software/Data Engineering Intern at Uber, I worked with the AdTech team, focusing on data engineering project.
+          My primary responsibility was to design and implement a new SQL table to centralize metadata storage for the team. 
+          This initiative aimed to eliminate redundant metadata across multiple tables in the codebase, thereby enhancing efficiency.
+          I designed the schema for the new table, wrote SQL queries to create it, and developed Python scripts to generate data pipelines that 
+          populate the table with data from various sources. Additionally, I implemented sensors to establish dependencies on the data sources, ensuring data integrity and consistency.
+          As a result of my work, the system became more streamlined, reducing the number of queries needed to access metadata. 
+          This improvement boosted the performance of the team&apos;s services and made it easier for the marketing team and other stakeholders to access the data they needed.
         </p>
-        <p className="text-sm font-light">UberSTAR Software Engineer Intern</p>
+        <p className="text-sm font-light">UberSTAR Software Engineering Intern</p>
         <p className="text-sm font-light italic">June 2023 - September 2023</p>
         <p className="text-sm font-light font-montez">
-          During my time as a UberSTAR Software Engineer Intern at Uber, I was responsible for leading initiatives such as load testing and deploying web services,
+          During my time as a UberSTAR Software Engineering Intern at Uber with the DOTCOM team, I was responsible for leading initiatives such as load testing and deploying web services,
           with the goal of enhancing the performance of Fusion.js. The primary objective of these initiatives was to improve server throughput while maintaining a 300ms latency and 1% error rate for P99.
           I developed a consistent and repeatable way to measure and analyze the performance of web applications at Uber by creating hypotheses and testing them to identify signals.
           I conducted research on systematic approaches for analyzing web server performance based on the 4 Golden Signals: Latency, Traffic, Errors, and Saturation.
@@ -50,6 +56,15 @@ export default function Experiences() {
           As a contractor with Robert Half, I played a crucial role in setting up and maintaining
           the Los Angeles County Voting Systems for multiple elections throughout 2020 and 2021.
           Additionally, I seized the opportunity to expertly de-rack and rack servers for T5 Data Center in El Segundo, CA.
+        </p>
+      </div>
+
+      <div className="mb-3 font-normal">
+        <strong>French Military / Butcher</strong>
+        <p className="text-sm font-light italic">August 2008 - December 2019</p>
+        <p className="text-sm font-light font-montez">
+          I have held various positions throughout my career before 2020, including serving in the French Military, and working as a butcher in France and Belgium.
+          These experiences have helped me develop a strong work ethic, problem-solving skills, and the ability to work effectively in a team in high-pressure environments.
         </p>
       </div>
     </div>
