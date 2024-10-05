5790f420d2e9c88463e81aacd369de87945543b2
Chris <rolletchris1@gmail.com>
2024-10-05 14:22:06 -0700

    fixed typo

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
 
           <div className="lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/astroid">
+              <a href="/projects/asteroid">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Near Earth Object, Daily Tracker</h5>
               </a>
               <p className="mb-3 font-normal text-gray-700">
