16ed03bb72d339f73dafc774514cf78086e4ebae
Chris <rolletchris1@gmail.com>
2024-11-02 11:38:16 -0700

    added qr code generator project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
 
-          <div className="lg:w-100 mb-4 lg:mb-0">
+          <div className="w-full lg:w-100 mb-4 lg:mb-16">
             <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
               <a href="/projects/asteroid">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Near Earth Object, Daily Tracker</h5>
@@ -29,7 +29,7 @@ export default function Page() {
             </div>
           </div>
 
-          <div className="lg:w-100 mb-4 lg:mb-0">
+          <div className="w-full lg:w-100 mb-4 lg:mb-16">
             <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
               <a href="/projects/resources">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Resource Library</h5>
@@ -43,7 +43,7 @@ export default function Page() {
             </div>
           </div>
 
-          <div className="lg:w-100 mb-4 lg:mb-12">
+          <div className="w-full lg:w-100 mb-4 lg:mb-16">
             <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
               <a href="/projects/cursif">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Cursif</h5>
@@ -57,7 +57,7 @@ export default function Page() {
             </div>
           </div>
 
-          <div className="lg:w-100 mb-4 lg:mb-16">
+          <div className="w-full lg:w-100 mb-4 lg:mb-16">
             <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
               <a href="/projects/deep-work">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Deep Work Timer</h5>
@@ -70,6 +70,21 @@ export default function Page() {
               </a>
             </div>
           </div>
+          
+          <div className="w-full lg:w-100 mb-4 lg:mb-16">
+            <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
+              <a href="/projects/qrcode">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">QR Code Generator</h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700">
+                Make your own QR Code quickly and easily, with full visibility and no concealed data.
+              </p>
+              <a href="/projects/qrcode" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                Try it! <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
+              </a>
+            </div>
+          </div>
+
 
         </div>
       </div>
---
+++ src/app/projects/qrcode/page.tsx
@@ -0,0 +1,64 @@
+"use client"
+import React, { useState } from "react";
+import Menu from "@/components/navigation";
+import Footer from "@/components/footer";
+import { QRCodeCanvas } from 'qrcode.react';
+
+export default function Page() {
+  const [url, setUrl] = useState("");
+  const [submittedUrl, setSubmittedUrl] = useState("");
+
+  const handleInputChange = (event: any) => {
+    setUrl(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    setSubmittedUrl(url);
+  };
+
+  return (
+    <div className="h-screen p-4 lg:p-8 mt-16">
+      <Menu />
+      <div className="p-4">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight">
+          <a href="/projects" className="underline">Projects</a>{" > "}QR Code
+        </h5>
+        <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
+
+        <form onSubmit={handleSubmit} className="my-4">
+          <label htmlFor="urlInput" className="block text-lg font-medium mb-2">
+            Enter URL:
+          </label>
+          <input
+            type="text"
+            id="urlInput"
+            placeholder="Enter URL to generate QR code"
+            value={url}
+            onChange={handleInputChange}
+            className="w-full p-2 border border-gray-300 rounded mb-2"
+          />
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Generate QR Code
+          </button>
+        </form>
+
+        <div className="flex justify-center my-4">
+          {submittedUrl && (
+            <QRCodeCanvas
+              value={submittedUrl}
+              size={200}
+              bgColor="#ffffff"
+              fgColor="#000000"
+              level="H"
+            />
+          )}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
