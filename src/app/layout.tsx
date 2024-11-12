f7813ec0e5f2de4b220cbd131fbb5ab780e65de2
Chris <rolletchris1@gmail.com>
2024-11-11 21:15:07 -0800

    [Update] updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,22 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-  title: 'Per Aspera Ad Astra',
-  description: 'Chris Dedman\'s Portfolio',
+  title: 'Chris Dedman\'s Portfolio',
+  description: 'Chris Dedman\'s Portfolio showcasing projects, skills, and experiences.',
+  openGraph: {
+    title: 'Chris Dedman\'s Portfolio',
+    description: 'Explore the work and projects of Chris Dedman, Software Engineer.',
+    url: 'https://chrisdedman.vercel.app',
+    type: 'website',
+    images: [
+      {
+        url: 'https://chrisdedman.vercel.app/_next/image?url=%2Fme.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Chris Dedman\'s Portfolio'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
@@ -18,10 +32,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className="min-w-[350px]">
-          {children}
-          <Analytics />
-          <SpeedInsights />
+        {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
