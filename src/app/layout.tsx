9b17052984bf19585b22a233e2d3f9796762606b
Chris <rolletchris1@gmail.com>
2024-11-22 16:12:25 -0800

    Updated Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,21 +7,21 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-  title: 'Chris Dedman\'s Portfolio',
-  description: 'Chris Dedman\'s Portfolio showcasing projects, skills, and experiences.',
   openGraph: {
     title: 'Chris Dedman\'s Portfolio',
-    description: 'Explore the work and projects of Chris Dedman, Software Engineer.',
+    description: 'Software Engineer, Chris Dedman\'s Portfolio showcasing projects, skills, and experiences.',
     url: 'https://chrisdedman.vercel.app',
-    type: 'website',
+    siteName: 'Chris Dedman\'s Portfolio',
     images: [
       {
-        url: '/website.jpeg',
+        url: 'https://chrisdedman.vercel.app/website.jpeg',
         width: 1200,
         height: 630,
         alt: 'Chris Dedman\'s Portfolio'
       }
-    ]
+    ],
+    locale: 'en_US',
+    type: 'website',
   }
 }
 
