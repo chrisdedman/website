8776741d5ab4c39744ca6d46a9cf22f1e893c836
Chris <rolletchris1@gmail.com>
2025-01-20 15:59:16 -0800

    Added research project description

---
+++ src/app/projects/research/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import React from "react";
+import Menu from '@/components/navigation'
+import Footer from '@/components/footer'
+
+export default function Research() {
+  return (
+    <div className="h-screen p-4 lg:p-8 mt-16 ">
+      <Menu />
+      <div className="p-4">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight">
+          <a href="/projects" className="underline">Achievements</a>{" > "}Research - Computational BioPhysics
+        </h5>
+        <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
+        <div>
+          <Article />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+function Article() {
+  return (
+    <>
+      <h6 className="mt-4 text-xl font-bold">A tool for the automatic generation of interaction beads in a coarse-grained model of a macromolecule.</h6>
+      <p className="mt-4">
+        This tool, developed as part of a Research, Scholarship, and Creative Activities (RSCA) grant,
+        is designed to provide a practical solution for overcoming the limitations of traditional multipole
+        expansions in coarse-graining electrostatic potentials. It implements a novel distributed multipole
+        expansion algorithm to accurately represent electrostatic potentials near the surfaces of biological molecules.
+      </p>
+
+      <p>
+        This tool integrates automatic geometric partitioning methods with Octree-based spatial structures to efficiently model biological molecules.
+        Its algorithms handle data parsing, merging partitions, and the iterative reduction of molecular complexity while preserving essential properties.
+        Creating coarse-grained models significantly improve computational efficiency, enabling large-scale simulations of complex biophysical systems.
+      </p>
+
+      <p>
+        <a href="research/tool"><strong>Get the Tool</strong></a>
+      </p>
+    </>
+  )
+} 
