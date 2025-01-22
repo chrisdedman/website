3eca32eeda8113195e5a3a6d15cfa8aa8fa1ac89
Chris <rolletchris1@gmail.com>
2025-01-21 21:31:24 -0800

    renamed projects to OS contributions

--- src/app/projects/cursif/page.tsx
+++ src/app/projects/cursif/page.tsx
@@ -11,7 +11,7 @@ export default function Cursif() {
       <Menu />
       <div className="p-4">
         <h5 className="mb-2 text-2xl font-bold tracking-tight">
-          <a href="/projects" className="underline">Projects</a>{" > "}Cursif
+          <a href="/projects" className="underline">Open Source Contributions</a>{" > "}Cursif
         </h5>
         <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
         <div>
