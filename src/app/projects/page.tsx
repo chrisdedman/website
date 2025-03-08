719414ee31d366523cbf545af649eb42aebc2fdc
Chris <rolletchris1@gmail.com>
2025-03-07 23:28:36 -0800

    Added CodeAstra card

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -29,6 +29,22 @@ export default function Page() {
               </div>
             </div>
           </div>
+
+          <div className="w-full lg:w-100 mb-4 lg:mb-16">
+            <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
+              <a href="/projects/cursif">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">CodeAstra</h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700">
+                CodeAstra is a fast, extensible, and lightweight C++/Qt6 code editor inspired by NeoVim and VSCode.
+              </p>
+              <div className="text-center">
+                <a href="/projects/code-astra" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                  Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
 
         <h5 className="mb-2 text-2xl font-bold tracking-tight">Projects</h5>
