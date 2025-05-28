365f1f52aff9cef6e3aa5c94bf935f398c43adb5
Chris <rolletchris1@gmail.com>
2025-05-27 18:20:38 -0700

    Update education section to include honors and clarify course details

--- src/components/resume/education.tsx
+++ src/components/resume/education.tsx
@@ -7,18 +7,19 @@ export default function Educations() {
       <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
       <div className="mb-3 font-normal">
         <strong>California State University, Dominguez Hills</strong>
-        <p className="text-sm font-light">Bachelor of Science, Computer Science</p>
+        <p className="text-sm font-light">Bachelor of Science, Computer Science (Graduated with Honor <b>Summa Cum Laude</b>)</p>
         <p className="text-sm font-light italic">January 2023 - May 2025</p>
         <p className="text-sm font-light font">
-          As a student at California State University, Dominguez Hills, I am pursuing a Bachelor of Science degree in Computer Science.
-          I have completed several computer science courses, including Software Development in java, Data Structure, Design and Analysis of Algorithms, Database Management and Computer Organisation.
-          I have also completed Probability & Statistics, and Physics 1 courses. I am currently taking Operating System, Software Engineering, and Physics 2 courses.
+          As a student at California State University, Dominguez Hills, I have completed several computer science courses, 
+          including Software Development in java, Data Structure, Design and Analysis of Algorithms, Database Management, 
+          Operating System, Software Engineering, Computer Organisation, Finite Automata, and Security Engineering.
+          I have also completed Probability & Statistics, and Physics 1 and 2 courses.
         </p>
       </div>
 
       <div className="mb-3 font-normal">
         <strong>El Camino College</strong>
-        <p className="text-sm font-light">Certificate of Achievement, Computer Science</p>
+        <p className="text-sm font-light">Certificate of Achievement with Honor, Computer Science</p>
         <p className="text-sm font-light italic">July 2021 - June 2023</p>
         <p className="text-sm font-light font">
           As a student at El Camino College, I have completed several computer science courses,
