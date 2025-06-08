91ed19a0576e865d0babe88a2830234747fd0c3e
Chris <rolletchris1@gmail.com>
2025-06-07 20:53:27 -0700

    Add footer component and enhance layout for improved presentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import Menu from "@/components/navigation";
+import Footer from '@/components/footer'
 
 const roles = ["a software engineer.", "a computer scientist.", "a problem solver.", "a critical thinker.", "chris dedman."];
 
@@ -24,19 +25,19 @@ export default function Page() {
         setDynamicText((prev) => prev + currentRole[charIndex]);
         setCharIndex((prev) => prev + 1);
       }
-      
+
       else if (isDeleting && charIndex > 0)
       {
         setDynamicText((prev) => prev.slice(0, -1));
         setCharIndex((prev) => prev - 1);
       }
-      
+
       else if (isDeleting && charIndex === 0)
       {
         setIsDeleting(false);
         setRoleIndex((prev) => (prev + 1) % roles.length);
       }
-      
+
       else if (!isDeleting && charIndex === currentRole.length)
       {
         setTimeout(() => setIsDeleting(true), delayBetweenRoles);
@@ -52,23 +53,38 @@ export default function Page() {
   }, [charIndex, isDeleting, roleIndex]);
 
   return (
-    <div
-      className="h-screen flex flex-col items-center justify-center"
-      style={{
-        backgroundImage: "url('/me_engine.jpeg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <Menu />
+    <>
+      <div
+        className="h-screen flex flex-col items-center justify-center"
+        style={{
+          backgroundImage: "url('/me_engine.jpeg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <Menu />
 
-      <div className="relative">
-        <div className="typing-container">
-          <span id="static-text">I am </span>
-          <span id="dynamic-text">{dynamicText}</span>
-          <span className="cursor">|</span>
+        <div className="w-full flex justify-end lg:pr-80 pr-8 mt-80">
+          <div className="typing-container">
+            <span id="static-text">I am </span>
+            <span id="dynamic-text">{dynamicText}</span>
+            <span className="cursor">|</span>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="flex-col p-4 lg:p-8 mt-16">
+        <div className="p-4 max-w-6xl mx-auto flex-grow bg-white rounded-lg shadow-md">
+          <p className="text-gray-800 leading-relaxed">
+            Christopher Dedman Rollet is a recent computer science graduate,
+            he is passionate about programming and eager to apply skills in an
+            entry-level software engineering role. With experience in various
+            programming languages and a focus on backend and embedded systems, Chris
+            seeks opportunities to grow and make an impact.
+          </p>
+        </div>
+        <Footer />
+      </div>
+    </>
   );
 }
