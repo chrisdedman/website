9167f7f472eb7fc2ea5678739c0d9766e3bb5f71
Chris <rolletchris1@gmail.com>
2025-11-17 17:15:12 -0800

    reorder OSS projetcs

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -20,18 +20,18 @@ export default function Page() {
         <h5 className="mb-2 text-2xl font tracking-tight">Open Source Contributions</h5>
         <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 items-stretch">
+
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/cursif">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Cursif</h5>
+              <a href="/projects/astra-kernel">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">AstraKernel</h5>
               </a>
               <p className="mb-3 text-gray-700">
-                A collaborative, scriptable note taking application. Cursif is
-                yet another note-taking application that adds an exciting twist
-                to your productivity.
+                AstraKernel is a minimal experimental kernel written in modern C
+                and ARM Assembly. Designed to run on QEMU&apos;s Versatile AB/PB board with a Cortex-A8 CPU override.
               </p>
               <div className="text-center">
-                <a href="/projects/cursif" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <a href="/projects/astra-kernel" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
                 </a>
               </div>
@@ -56,16 +56,16 @@ export default function Page() {
 
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/astra-kernel">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">AstraKernel</h5>
+              <a href="/projects/cursif">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Cursif</h5>
               </a>
               <p className="mb-3 text-gray-700">
-                AstraKernel is a minimal experimental kernel written in modern C
-                and ARM Assembly. Designed to run on QEMU&#39;s VersatilePB
-                (ARM926EJ-S) emulated platform.
+                A collaborative, scriptable note taking application. Cursif is
+                yet another note-taking application that adds an exciting twist
+                to your productivity.
               </p>
               <div className="text-center">
-                <a href="/projects/astra-kernel" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <a href="/projects/cursif" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
                 </a>
               </div>
