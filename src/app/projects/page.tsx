b3767b018eeadd898e3dd4e3e125e16d2b79cdfe
Chris <rolletchris1@gmail.com>
2026-01-17 18:38:08 -0800

    use component for project layout, and change <a> to <Link>

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,13 @@
 "use client"
 
 import React from "react";
-import Menu from '@/components/navigation'
-import Footer from '@/components/footer'
+import Link from 'next/link';
 import { CursorArrowRaysIcon } from "@heroicons/react/24/solid";
+import ProjectLayout from '@/components/project-layout';
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex-col p-4 lg:p-8 mt-16">
-      <Menu />
-      <div className="p-4 max-w-6xl mx-auto flex-grow">
+    <ProjectLayout>
         {
         /*
          ************************************
@@ -23,85 +21,85 @@ export default function Page() {
 
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/astra-kernel">
+              <Link href="/projects/astra-kernel">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">AstraKernel</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 AstraKernel is a minimal experimental kernel written in modern C
                 and ARM Assembly. Designed to run on QEMU&apos;s Versatile AB/PB board with a Cortex-A8 CPU override.
               </p>
               <div className="text-center">
-                <a href="/projects/astra-kernel" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/astra-kernel" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/code-astra">
+              <Link href="/projects/code-astra">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">CodeAstra</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 CodeAstra is a fast, extensible, and lightweight C++/Qt6 code editor inspired by NeoVim and VSCode.
               </p>
               <div className="text-center">
-                <a href="/projects/code-astra" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/code-astra" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/cursif">
+              <Link href="/projects/cursif">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Cursif</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 A collaborative, scriptable note taking application. Cursif is
                 yet another note-taking application that adds an exciting twist
                 to your productivity.
               </p>
               <div className="text-center">
-                <a href="/projects/cursif" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/cursif" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-12">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/matrixpy">
+              <Link href="/projects/matrixpy">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Matrix.py</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 Matrix.py is a lightweight and intuitive Python library to build bots
                 on the Matrix protocol. It provides a clean, decorator-based API
                 similar to popular event-driven frameworks, allowing developers to focus on behavior rather than boilerplate.
               </p>
               <div className="text-center">
-                <a href="/projects/matrixpy" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/matrixpy" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
           
           <div className="w-full lg:w-100 mb-16 lg:mb-12">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/capski">
+              <Link href="/projects/capski">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Capski</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 Capski is a Rust-based command-line tool for creating karaoke-style videos with real-time subtitles.
               </p>
               <div className="text-center">
-                <a href="/projects/capski" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/capski" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -119,43 +117,43 @@ export default function Page() {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 items-stretch">
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/astra-pulse">
+              <Link href="/projects/astra-pulse">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">AstraPulse</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 A minimalist desktop native timer app designed for simplicity and ease of use.
               </p>
               <div className="text-center">
-                <a href="/projects/astra-pulse" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/astra-pulse" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/asteroid">
+              <Link href="/projects/asteroid">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Near Earth Object, Daily Tracker</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 This web app allows users to retrieve and display daily
                 information about Near Earth Objects (NEOs). NEOs are asteroids
                 and comets with orbits that come close to Earth&apos;s orbit.
               </p>
               <div className="text-center">
-                <a href="/projects/asteroid" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/asteroid" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-0">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/resources">
+              <Link href="/projects/resources">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Resource Library</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 This project is a web page that allows users to search for
                 resources by category and keyword. The user can also add new
@@ -163,59 +161,59 @@ export default function Page() {
                 which then will be displayed on the webpage.
               </p>
               <div className="text-center">
-                <a href="/projects/resources" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/resources" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Read More <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-12">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/deep-work">
+              <Link href="/projects/deep-work">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Deep Work Timer</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 Deep Working is a technique that helps you focus on a single
                 task for a set amount of time. This timer helps you do just that!
               </p>
               <div className="text-center">
-                <a href="/projects/deep-work" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/deep-work" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Try it! <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-12">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/qrcode">
+              <Link href="/projects/qrcode">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">QR Code Generator</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 Make your own QR Code quickly and easily, with full visibility
                 and no concealed data.
               </p>
               <div className="text-center">
-                <a href="/projects/qrcode" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/qrcode" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Try it! <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-16 lg:mb-12">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/crud-template">
+              <Link href="/projects/crud-template">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">CRUD Web Template</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 A boilerplate template for quickly setting up a full-stack web application using Go with backend APIs, database integration, and frontend setup.
               </p>
               <div className="text-center">
-                <a href="/projects/crud-template" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/crud-template" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Try it! <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -233,9 +231,9 @@ export default function Page() {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 items-stretch">
           <div className="w-full lg:w-100 mb-4 lg:mb-8">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/research">
+              <Link href="/projects/research">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Research - Computational BioPhysics</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 Contributed to the development of a computational tool for
                 automating the geometric partitioning of biological molecules
@@ -244,18 +242,18 @@ export default function Page() {
                 optimization of algorithm and software implementation in Java.
               </p>
               <div className="text-center">
-                <a href="/projects/research" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/research" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Learn more! <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
           <div className="w-full lg:w-100 mb-4 lg:mb-8">
             <div className="max-w-sm h-full flex flex-col justify-between p-6 bg-white border border-gray-200 rounded-lg shadow mt-8">
-              <a href="/projects/research-ai">
+              <Link href="/projects/research-ai">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">Research - AI/ML</h5>
-              </a>
+              </Link>
               <p className="mb-3 text-gray-700">
                 Classify cancer subtypes using gene expression data.
                 Unsupervised learning techniques, such as clustering and
@@ -264,16 +262,13 @@ export default function Page() {
                 Pan-Cancer dataset.
               </p>
               <div className="text-center">
-                <a href="/projects/research-ai" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                <Link href="/projects/research-ai" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
                   Learn more! <CursorArrowRaysIcon className="w-4 h-4 ms-2" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <Footer />
-    </div>
+    </ProjectLayout>
   );
 }
